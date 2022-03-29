79d23f8ee406a0a7577a0443609b835de21aca24
Michael Harkess <56043036+mharkess@users.noreply.github.com>
2022-03-29 14:21:57 -0400

    Main.js API URL update
    Changed API URL to fix request issues on most browsers

--- EcoSwitch Website/src/pages/Main.js
+++ EcoSwitch Website/src/pages/Main.js
@@ -20,7 +20,7 @@ export default class Main extends React.Component {
     // }, []);
 
 toggleButtonState = () => {
-    var url = 'https://3.12.233.95/allRecent.php?deviceID=12345';
+    var url = 'https://tyd5faaoq0.execute-api.us-east-2.amazonaws.com/Test/allRecent?deviceID=12345';
 
     fetch(url)
         .then((res) => {
